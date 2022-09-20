ddf47074b0960ea1fca9729aa9f5323f8f0205b6
Joswin Emmanuel <96587885+joswinemmanuel@users.noreply.github.com>
2022-09-20 19:03:10 +0530

    Add files via upload

---
+++ Ratio.java
@@ -0,0 +1,9 @@
+public class Ratio {
+    public static void main(String[] args) {
+        int a=20, b=10;
+        float r = (float)a / (float)b;
+        System.out.println("Ratio is : " + r);
+    }
+}
+
+// Ratio is : 2.0
