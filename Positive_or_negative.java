d3faa7404fb00827f4dba7e465d5b8386b8262eb
Joswin Emmanuel <96587885+joswinemmanuel@users.noreply.github.com>
2022-09-20 19:34:00 +0530

    Add files via upload

---
+++ Positive_or_negative.java
@@ -0,0 +1,14 @@
+public class Positive_or_negative {
+    public static void main(String[] args){
+        int n = 10;
+        if(n > 0){
+            System.out.println("Positive");
+        } else if(n < 0){
+            System.out.println("Negative");
+        } else {
+            System.out.println("Zero");
+        }
+    }
+}
+
+// Positive
