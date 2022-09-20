a8e6412ad928acf10a0197e209410ade4004ce38
Joswin Emmanuel <96587885+joswinemmanuel@users.noreply.github.com>
2022-09-20 18:54:06 +0530

    Add files via upload

---
+++ Operations.java
@@ -0,0 +1,10 @@
+public class Operations {
+    public static void main(String[] args) {
+        int a = 20;
+        int b = 10;
+        System.out.println("Sum = "+ (a+b));
+        System.out.println("Difference = " + (a-b));
+        System.out.println("Product = " + (a*b));
+        System.out.println("Quotient = " + (a/b));
+    }
+}
