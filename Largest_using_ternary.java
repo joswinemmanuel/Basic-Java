fbd8701e1eb45e9b81fd7b2021e73a4aeccd2709
Joswin Emmanuel <96587885+joswinemmanuel@users.noreply.github.com>
2022-09-20 19:00:17 +0530

    Add files via upload

---
+++ Largest_using_ternary.java
@@ -0,0 +1,9 @@
+public class Largest_using_ternary {
+    public static void main(String[] args){
+        int a=100, b=200;
+        int c = (a>b) ? a : b;
+        System.out.println("Largest is : " + c);
+    }
+}
+
+// Largest is : 200
