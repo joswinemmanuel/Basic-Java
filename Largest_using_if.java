a0bc12c5edb1e18dcb9f13bcb508908b32b6849e
Joswin Emmanuel <96587885+joswinemmanuel@users.noreply.github.com>
2022-09-20 19:07:38 +0530

    Add files via upload

---
+++ Largest_using_if.java
@@ -0,0 +1,12 @@
+public class Largest_using_if {
+    public static void main(String[] args) {
+        int a=100, b=200;
+        if(a > b){
+            System.out.println("Largest : " + a);
+        } else {
+            System.out.println("Largest : " + b);
+        }
+    }
+}
+
+// Largest : 200
