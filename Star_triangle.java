0fa375160ae6be2c5b82ab993ca4f2eb4799167d
Joswin Emmanuel <96587885+joswinemmanuel@users.noreply.github.com>
2022-09-20 20:01:08 +0530

    Add files via upload

---
+++ Star_triangle.java
@@ -0,0 +1,21 @@
+public class Star_triangle {
+    public static void main(String[] args){
+        int n=5;
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=i; j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
+
+/*
+
+* 
+* *
+* * *
+* * * *
+* * * * * 
+
+*/
