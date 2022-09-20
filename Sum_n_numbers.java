d3138274cd8966cacee21c581cdb549753ed926f
Joswin Emmanuel <96587885+joswinemmanuel@users.noreply.github.com>
2022-09-20 19:43:28 +0530

    Add files via upload

---
+++ Sum_n_numbers.java
@@ -0,0 +1,11 @@
+public class Sum_n_numbers {
+    public static void main(String[] args){
+        int sum=0, n=5;
+        for(int i=0; i<=n; i++){
+            sum += i;
+        }
+        System.out.println("Sum is : " + sum);
+    }
+}
+
+// Sum is : 15
