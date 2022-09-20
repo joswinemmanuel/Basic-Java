5cd572a92a6b718a6ac5890214e6ff8e8d50b08c
Joswin Emmanuel <96587885+joswinemmanuel@users.noreply.github.com>
2022-09-20 19:29:51 +0530

    Add files via upload

---
+++ Odd_or_even.java
@@ -0,0 +1,12 @@
+public class Odd_or_even {
+    public static void main(String[] args){
+        int n = 9;
+        if(n%2 == 0){
+            System.out.println("EVEN");
+        } else {
+            System.out.println("ODD");
+        }
+    }
+}
+
+// ODD
