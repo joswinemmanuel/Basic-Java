c2fe26419281ebccbfa3588c777dca13b0e402fd
Joswin Emmanuel <96587885+joswinemmanuel@users.noreply.github.com>
2022-09-20 19:26:59 +0530

    Add files via upload

---
+++ Day_of_week.java
@@ -0,0 +1,32 @@
+public class Day_of_week {
+    public static void main(String[] args){
+        int n = 5;
+        switch(n){
+            case 1:
+                System.out.println("SUNDAY");
+                break;
+            case 2:
+                System.out.println("MONDAY");
+                break;
+            case 3:
+                System.out.println("TUESDAY");
+                break;
+            case 4:
+                System.out.println("WEDNESDAY");
+                break;
+            case 5:
+                System.out.println("THURSDAY");
+                break;
+            case 6:
+                System.out.println("FRIDAY");
+                break;
+            case 7:
+                System.out.println("SATURDAY");
+                break;
+            default:
+                System.out.println("Invalid input");
+        }
+    }
+}
+
+// THURSDAY
