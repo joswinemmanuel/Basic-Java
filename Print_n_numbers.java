3e3c3b09015196130fab7f3f9ede9b2cd5f5d048
Joswin Emmanuel <96587885+joswinemmanuel@users.noreply.github.com>
2022-09-20 19:41:40 +0530

    Add files via upload

---
+++ Print_n_numbers.java
@@ -0,0 +1,22 @@
+public class Print_n_numbers {
+    public static void main(String[] args) {
+        int i=1, n=10;
+        while(i <= n){
+            System.out.println(i);
+            i++;
+        }
+    }
+}
+
+/*
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+*/
