7ab0106cdbf1d9c97a2641ad166279657bfc0297
Joswin Emmanuel <96587885+joswinemmanuel@users.noreply.github.com>
2022-09-20 19:14:51 +0530

    Add files via upload

---
+++ Equal_numbers.java
@@ -0,0 +1,12 @@
+public class Equal_numbers {
+    public static void main(String[] args){
+        int a=100, b=100;
+        if(a == b){
+            System.out.println("Equal");
+        } else {
+            System.out.println("Not equal");
+        }
+    }
+}
+
+// Equal
