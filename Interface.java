e4db4466b7389eb9954b1fe447536354dff2459f
Joswin Emmanuel <96587885+joswinemmanuel@users.noreply.github.com>
2022-11-02 20:01:50 +0530

    Interface in Java

---
+++ Interface.java
@@ -0,0 +1,18 @@
+public class Interface {
+    public static void main(String[] args) {
+        PrintData obj1 = new PrintData();
+        obj1.print_hello();
+    }
+}
+
+interface Display {
+    void print_hello();
+}
+
+class PrintData implements Display {
+    public void print_hello() {
+        System.out.println("Hello");
+    }
+}
+
+// Hello
